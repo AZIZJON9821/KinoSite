9f99a09cd367588bf613ea0c3f25d3c3b49d945d
AZIZ <bozorboyevazizjon56@gmail.com>
2026-01-28 13:56:13 +0500

    .

--- hooks/useUsers.ts
+++ hooks/useUsers.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { customAxios } from "@/api/instances";
+import { customAxios } from "@/api-client/instances";
 import { useSession } from "next-auth/react";
 import { toast } from "react-hot-toast";
 
